893bff6911623184a0a3b604ed441f633568e8de
Konan KOUAME <fabrice.fkouame@gmail.com>
2023-07-31 14:56:53 +0200

    added serviceAccountKey.json file and made migrations

--- picappAPI2/settings.py
+++ picappAPI2/settings.py
@@ -40,17 +40,24 @@ INSTALLED_APPS = [
     "django_extensions",
     'rest_framework',
     "corsheaders",
+    'roompic',
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # "corsheaders.middleware.CorsMiddleware",
+    # "django.middleware.common.CommonMiddleware",
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'picappAPI2.urls'
 
@@ -78,10 +85,10 @@ WSGI_APPLICATION = 'picappAPI2.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
